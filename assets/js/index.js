683442b200941a1beb64e88e82b426ea3d982640
alan-chala <alan.chala000@gmail.com>
2026-06-25 14:26:21 -0500

    feat: implement turn system and restart game functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,32 @@ startTheGameButton.addEventListener('click', () => {
   startTheGameContainer.style.display = 'none';
   startTheGameContainer.setAttribute('aria-hidden', 'true');
 });
+
+const cellElements = document.querySelectorAll('.cell');
+
+let turn = 'X';
+
+cellElements.forEach((cell) => {
+  cell.addEventListener('click', () => {
+    if (cell.textContent === '') {
+      cell.textContent = turn;
+      cell.setAttribute('aria-label', turn);
+      cell.setAttribute('aria-pressed', 'true');
+      turn = turn === 'X' ? 'O' : 'X';
+      document.querySelector('.show-turn').classList.toggle('right');
+    }
+  });
+});
+
+const restartGameButton = document.getElementById('restartGame');
+
+restartGameButton.addEventListener('click', () => {
+  cellElements.forEach((cell) => {
+    cell.textContent = '';
+    cell.setAttribute('aria-label', '');
+    cell.setAttribute('aria-pressed', 'false');
+  });
+  turn = 'X';
+  document.querySelector('.show-turn').classList.remove('right');
+});
+
